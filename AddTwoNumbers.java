307a0f7849a298ef36db24873e6f0a2014b3bf74
vicky <prajwalkawale01@hotmail.com>
2024-03-18 16:31:33 +0530

    java practice

---
+++ AddTwoNumbers.java
@@ -0,0 +1,27 @@
+public class AddTwoNumbers {
+
+     public static void main(String[] args) {
+         int a = 10;
+         int b =10;
+         int sum = a +b;
+         System.out.println(sum);
+
+         // Data Types
+         int age = 45;
+         byte age1 = 44;
+         short age2= 456;
+         long age6 =454545455545L;
+         float age3 =150.2f;
+         double age4 =120222;
+         char age5='a';
+         String user = "Programmer";
+         System.out.println(age);
+         System.out.println(age1);
+         System.out.println(age2);
+         System.out.println(age3);
+         System.out.println(age4);
+         System.out.println(age5);
+         System.out.println(age6);
+         System.out.println(user);
+    }
+}
